3082258ebbabf32989e848cc5d6603e37ed987de
chirawat.not@gmail.com <chirawat.not@gmail.com>
2016-11-14 16:01:14 +0700

    update dd pos

--- sample_data/dd_pos.sql
+++ sample_data/dd_pos.sql
@@ -1,7 +1,12 @@
-id,position
-1,หน้า
-2,ข้างซ้าย
-3,ข้างขวา
-4,บน
-5,ล่าง
-6,หลัง
+/*
+-- Query: SELECT * FROM garage.damage_position
+LIMIT 0, 1000
+
+-- Date: 2016-11-14 16:00
+*/
+INSERT INTO `damage_position` (`id`,`position`) VALUES (1,'หน้า');
+INSERT INTO `damage_position` (`id`,`position`) VALUES (2,'ข้างซ้าย');
+INSERT INTO `damage_position` (`id`,`position`) VALUES (3,'ข้างขวา');
+INSERT INTO `damage_position` (`id`,`position`) VALUES (4,'บน');
+INSERT INTO `damage_position` (`id`,`position`) VALUES (5,'ล่าง');
+INSERT INTO `damage_position` (`id`,`position`) VALUES (6,'หลัง');

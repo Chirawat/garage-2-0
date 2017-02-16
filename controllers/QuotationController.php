e4dcbaa7d6c5ae6ddea49fe3f38318cebef97d3d
chirawat.not@gmail.com <chirawat.not@gmail.com>
2017-02-16 11:51:52 +0700

    Fix quotation report bug

--- controllers/QuotationController.php
+++ controllers/QuotationController.php
@@ -423,7 +423,7 @@ class QuotationController extends Controller
             // stream to browser inline
             'destination' => Pdf::DEST_BROWSER, 
                 
-            'marginTop' => 114,
+            'marginTop' => 118,
                 
             'marginBottom' => 50,
                 
